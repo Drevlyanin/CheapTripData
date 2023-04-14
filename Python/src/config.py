038d661ca3b961d0045751eaef6508f0db6e8438
udawe78 <udawe78@gmail.com>
2023-04-15 02:26:45 +0300

    save images

--- Python/src/config.py
+++ Python/src/config.py
@@ -94,5 +94,8 @@ LOGS_DIR = Path('../logs')
 LOG_CRITICAL = Path(LOGS_DIR/'critical_errors.log')
 LOG_CRITICAL_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 
-#prompts set up
+# prompts set up
 PROMPTS_JSON = Path('../files/seo/prompts.json')
+
+# directory for images from pexel
+PEXEL_IMG_DIR = Path('../output/cities_info/images')
